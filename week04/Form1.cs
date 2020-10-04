52356bc91324b0b5966a35df6da271448a13c668
Dávid Benedek Csontos <david.csontos2@stud.uni-corvinus.hu>
2020-10-04 20:44:11 +0200

    Kimenet formazas, hibajavitasok

--- week04/Form1.cs
+++ week04/Form1.cs
@@ -10,110 +10,155 @@ using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
+
 namespace week04
 {
-    public partial class Form1 : Form
-    {
-        RealEstateEntities context = new RealEstateEntities();
-        List<Flat> Flats;
+	public partial class Form1 : Form
+	{
 
-        Excel.Application xlApp; 
-        Excel.Workbook xlWB; 
-        Excel.Worksheet xlSheet; 
+		List<Flat> Flats;
+		RealEstateEntities context = new RealEstateEntities();
 
-        public Form1()
-        {
-            InitializeComponent();
-            LoadData();
+		Excel.Application xlApp;
+		Excel.Workbook xlWB;
+		Excel.Worksheet xlSheet;
 
-            try
-            {
-                xlApp = new Excel.Application();
+		string[] headers = new string[]
+			{
+				"Kód",
+				"Eladó",
+				"Oldal",
+				"Kerület",
+				"Lift",
+				"Szobák száma",
+				"Alapterület (m2)",
+				"Ár (mFt)",
+				"Négyzetméter ár (Ft/m2)"
+			};
+		public Form1()
+		{
+			InitializeComponent();
+			LoadData();
+			CreateExcel();
 
-                xlWB = xlApp.Workbooks.Add(Missing.Value);
+		}
 
-                xlSheet = xlWB.ActiveSheet;
+		private void LoadData()
+		{
+			Flats = context.Flats.ToList();
+		}
 
-                
-                //CreateTable(); 
+		private void CreateExcel()
+		{
+			try
+			{
+				xlApp = new Excel.Application();
+				xlWB = xlApp.Workbooks.Add(Missing.Value);
+				xlSheet = xlWB.ActiveSheet;
 
-                xlApp.Visible = true;
-                xlApp.UserControl = true;
-            }
-            catch (Exception ex) 
-            {
-                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
-                MessageBox.Show(errMsg, "Error");
+				CreateTable();
+				FormatTable();
 
-                xlWB.Close(false, Type.Missing, Type.Missing);
-                xlApp.Quit();
-                xlWB = null;
-                xlApp = null;
-            }
-        }
+				xlApp.Visible = true;
+				xlApp.UserControl = true;
 
-        private void LoadData()
-        {
-            Flats = context.Flats.ToList();
-        }
+			}
+			catch (Exception ex)
+			{
+				string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+				MessageBox.Show(errMsg, "Error");
 
-        void CreateTable()
-        {
-            string[] headers = new string[] {
-                "Kód",
-                "Eladó",
-                "Oldal",
-                "Kerület",
-                "Lift",
-                "Szobák száma",
-                "Alapterület (m2)",
-                "Ár (mFt)",
-                "Négyzetméter ár (Ft/m2)"};
+				xlWB.Close(false, Type.Missing, Type.Missing);
+				xlApp.Quit();
+				xlWB = null;
+				xlApp = null;
 
-            for (int i = 0; i < headers.Length; i++)
-            {
-                xlSheet.Cells[1, i + 1] = headers[i];
-            }
+			}
 
-            object[,] values = new object[Flats.Count, headers.Length];
-            int counter = 0;
-            foreach (Flat f in Flats)
-            {
-                values[counter, 0] = f.Code;
-                values[counter, 1] = f.Vendor;
-                values[counter, 2] = f.Side;
-                values[counter, 3] = f.District;
-                values[counter, 4] = f.Elevator;
-                values[counter, 5] = f.NumberOfRooms;
-                values[counter, 6] = f.FloorArea;
-                values[counter, 7] = f.Price;
-                values[counter, 8] = "";
-                counter++;
-            }
+		}
 
-            xlSheet.get_Range(
-             GetCell(2, 1),
-             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
-
-        }
-
-        private string GetCell(int x, int y)
-        {
-            string ExcelCoordinate = "";
-            int dividend = y;
-            int modulo;
-
-            while (dividend > 0)
-            {
-                modulo = (dividend - 1) % 26;
-                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
-                dividend = (int)((dividend - modulo) / 26);
-            }
-            ExcelCoordinate += x.ToString();
-
-            return ExcelCoordinate;
+		private void CreateTable()
+		{
 
 
-        }
-    }
+			for (int i = 1; i <= headers.Length; i++)
+			{
+				xlSheet.Cells[1, i] = headers[i - 1];
+			}
+
+			object[,] values = new object[Flats.Count, headers.Length];
+
+			int counter = 0;
+			foreach (Flat f in Flats)
+			{
+				values[counter, 0] = f.Code;
+				values[counter, 1] = f.Vendor;
+				values[counter, 2] = f.Side;
+				values[counter, 3] = f.District;
+				if (f.Elevator == true)
+				{
+					values[counter, 4] = "Van";
+
+				}
+				else
+				{
+					values[counter, 4] = "Nincs";
+				}
+
+				values[counter, 5] = f.NumberOfRooms;
+				values[counter, 6] = f.FloorArea;
+				values[counter, 7] = f.Price;
+				values[counter, 8] = "=" + (GetCell(counter + 2, values.GetLength(1) - 2)).ToString() + "*" + (GetCell(counter + 2, values.GetLength(1) - 1)).ToString();
+				counter++;
+			}
+
+			xlSheet.get_Range(GetCell(2, 1), GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+
+		}
+
+		private string GetCell(int x, int y)
+		{
+			string ExcelCoordinate = "";
+			int dividend = y;
+			int modulo;
+
+			while (dividend > 0)
+			{
+				modulo = (dividend - 1) % 26;
+				ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+				dividend = (int)((dividend - modulo) / 26);
+			}
+
+			ExcelCoordinate += x.ToString();
+
+			return ExcelCoordinate;
+		}
+
+		private void FormatTable()
+		{
+			Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+			headerRange.Font.Bold = true;
+			headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+			headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+			headerRange.EntireColumn.AutoFit();
+			headerRange.RowHeight = 40;
+			headerRange.Interior.Color = Color.LightBlue;
+			headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+
+			int lastRowID = xlSheet.UsedRange.Rows.Count;
+
+			Excel.Range tableRange = xlSheet.get_Range(GetCell(1, 1), GetCell(lastRowID, headers.Length));
+			tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+			Excel.Range firstRow = xlSheet.get_Range(GetCell(2, 1), GetCell(lastRowID, 1));
+			firstRow.Font.Bold = true;
+			firstRow.Interior.Color = Color.LightYellow;
+
+			Excel.Range lastRow = xlSheet.get_Range(GetCell(2, headers.Length), GetCell(lastRowID, headers.Length));
+			lastRow.Interior.Color = Color.LightGreen;
+			lastRow.NumberFormat = "#.00";
+		}
+	}
 }
