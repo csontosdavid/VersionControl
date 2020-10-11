a35243efb97f764753d4c8766274445d6f5c0cff
Dávid Benedek Csontos <david.csontos2@stud.uni-corvinus.hu>
2020-10-11 21:52:51 +0200

    VaR kiszamitasa

--- VAR/Form1.cs
+++ VAR/Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VAR.Entities;
+
 
 namespace VAR
 {
@@ -40,6 +42,8 @@ namespace VAR
 
 		}
 
+		
+
 		private decimal GetPortfolioValue(DateTime date)
 		{
 			decimal value = 0;
@@ -47,12 +51,65 @@ namespace VAR
 			{
 				var last = (from x in Ticks
 							where item.Index == x.Index.Trim()
-							   && date <= x.TradingDay
+							&& date <= x.TradingDay
 							select x)
 							.First();
 				value += (decimal)last.Price * item.Volume;
 			}
+
 			return value;
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Filter = "Comma Separated Values (*.csv)|*.csv";
+
+			if (sfd.ShowDialog() != DialogResult.OK) return;
+
+			sfd.AddExtension = true;
+			sfd.Title = "save";
+
+			if (sfd.FileName != "")
+			{
+				List<decimal> Nyereségek = new List<decimal>();
+				int intervalum = 30;
+				DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+				DateTime záróDátum = new DateTime(2016, 12, 30);
+				TimeSpan z = záróDátum - kezdőDátum;
+				for (int i = 0; i < z.Days - intervalum; i++)
+				{
+					decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                        - GetPortfolioValue(kezdőDátum.AddDays(i));
+					Nyereségek.Add(ny);
+					Console.WriteLine(i + "" + ny);
+				}
+
+				var nyereségekRendezve = (from x in Nyereségek
+										  orderby x
+										  select x)
+										  .ToList();
+				MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
+
+				using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+				{
+					sw.WriteLine("Időszak; Nyereség");
+
+					for (int i = 0; i < nyereségekRendezve.Count; i++)
+					{
+						sw.WriteLine(i + ";" + nyereségekRendezve[i]);
+					}
+
+				}
+
+			}
+
+		}
+
+
 	}
+
+
 }
