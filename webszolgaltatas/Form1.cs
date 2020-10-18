06c509d2661204027295ecf67d8920a726d85ae4
Dávid Benedek Csontos <david.csontos2@stud.uni-corvinus.hu>
2020-10-18 21:54:27 +0200

    Árfolyam lista létrehozás Commit

---
+++ webszolgaltatas/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using webszolgaltatas.MnbServiceReference;
+using webszolgaltatas.Entities;
+
+namespace webszolgaltatas
+{
+    
+
+    public partial class Form1 : Form
+    {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = Rates;
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            
+        }
+    }
+}
